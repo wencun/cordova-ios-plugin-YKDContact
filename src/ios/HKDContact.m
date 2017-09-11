8da0272c367c64c928aff764f3d24eac59a35216
wencun <wencun@users.noreply.github.com>
2017-09-11 10:48:04 +0800

    update

--- src/ios/HKDContact.m
+++ src/ios/HKDContact.m
@@ -12,7 +12,7 @@
 @interface HKDContact()<CNContactPickerDelegate>
 
 @property (nonatomic, copy) NSString *callbackId;
-@property (nonatomic, strong) NSMutableArray *array;
+@property (nonatomic, strong) NSMutableDictionary *contactDictionary;
 
 @end
 
@@ -22,7 +22,7 @@
     NSDictionary *dict  = [command argumentAtIndex:0 withDefault:nil];
     if (dict) {
         _callbackId = [command.callbackId copy];
-        self.array = [NSMutableArray array];
+        self.contactDictionary = [NSMutableDictionary dictionary];
         // 判断是否授权
         //让用户给权限,没有的话会被拒的各位
         CNAuthorizationStatus status = [CNContactStore authorizationStatusForEntityType:CNEntityTypeContacts];
@@ -30,10 +30,11 @@
             CNContactStore *store = [[CNContactStore alloc] init];
             [store requestAccessForEntityType:CNEntityTypeContacts completionHandler:^(BOOL granted, NSError * _Nullable error) {
                 if (error) {
-                    NSLog(@"weishouquan ");
+                    NSLog(@"您未开启通讯录权限,请前往设置中心开启 ");
+                    [self getContactInfo:NO];
                 }else
                 {
-                    NSLog(@"chenggong ");//用户给权限了
+                    NSLog(@"用户给权限了 ");//用户给权限了
                     CNContactPickerViewController * picker = [CNContactPickerViewController new];
                     picker.delegate = self;
                     picker.displayedPropertyKeys = @[CNContactPhoneNumbersKey];//只显示手机号
@@ -50,20 +51,46 @@
         }
         else{
             @"您未开启通讯录权限,请前往设置中心开启";
+            [self getContactInfo:NO];
         }
         
     }
 }
 
+#pragma mark - 点击某个联系人的某个属性（property）时触发并返回该联系人属性（contactProperty）。
+//只实现该方法时，可以进入到联系人详情页面（如果predicateForSelectionOfProperty属性没被设置或符合筛选条件，如不符合会触发默认操作，即打电话，发邮件等）。
+- (void)contactPicker:(CNContactPickerViewController *)picker didSelectContactProperty:(CNContactProperty *)contactProperty {
+    NSLog(@"%@",contactProperty);
+    CNContact *contact = contactProperty.contact;
+    NSLog(@"givenName: %@, familyName: %@", contact.givenName, contact.familyName);
+    NSString *name = [NSString stringWithFormat:@"%@%@",contact.familyName,contact.givenName];
+    if (![contactProperty.value isKindOfClass:[CNPhoneNumber class]]) {
+        [[HNPublicTool shareInstance] showHudErrorMessage:@"请选择11位手机号"];
+        return;
+    }
+    CNPhoneNumber *phoneNumber = contactProperty.value;
+    NSString * Str = phoneNumber.stringValue;
+    NSCharacterSet *setToRemove = [[NSCharacterSet characterSetWithCharactersInString:@"0123456789"] invertedSet];
+    NSString *phoneStr = [[Str componentsSeparatedByCharactersInSet:setToRemove]componentsJoinedByString:@""];
+    if (phoneStr.length != 11) {
+        [[HNPublicTool shareInstance] showHudErrorMessage:@"请选择11位手机号"];
+    }
+    NSLog(@"-=-=%@",phoneStr);
+    [contactDictionary setObject:name forKey:@"name"];
+    [contactDictionary setObject:phoneStr forKey:@"phone"];
+    
+    [self getContactInfo:YES];
+    
+//    self.phoneTextView.text = phoneStr;
+}
+
 - (void)getContactInfo:(BOOL)isGranted{
     NSMutableDictionary *dict = [NSMutableDictionary dictionary];
     if(isGranted){
-        NSData *data = [NSJSONSerialization dataWithJSONObject:self.array options:NSJSONWritingPrettyPrinted error:nil];
-        NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        [dict setObject:str forKey:@"contacts"];
-        [dict setObject:@(self.array.count) forKey:@"totalCount"];
+        dict = _contactDictionary;
     }else{
-        [dict setObject:@(-1) forKey:@"totalCount"];
+        [contactDictionary setObject:@"" forKey:@"name"];
+        [contactDictionary setObject:@"" forKey:@"phone"];
     }
     CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dict];
     [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
